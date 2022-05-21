687bb7b2e6956b6a89ffb7beb636412a1a97b3ef
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-05-21 21:10:31 +0300

    refactor: add load example todos methods, update todoList as nullable, remove example todolist from ngOnInit

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -3,6 +3,10 @@ import {
   faSquareMinus,
   faSquarePlus,
 } from '@fortawesome/free-regular-svg-icons';
+import {
+  getCareerPathTodos,
+  getDotnetFullStackTodos,
+} from 'src/app/datas/exampleTodos';
 import { TaskStatus, Todo } from 'src/app/models/Todo';
 
 @Component({
@@ -17,8 +21,9 @@ export class TodosComponent implements OnInit {
   faSquarePlus = faSquarePlus;
 
   inputTodo: string = '';
+  todoList: Todo[] = [];
 
-  todoList!: Todo[];
+  ngOnInit(): void {}
 
   addTodo(): void {
     if (this.inputTodo === '') return;
@@ -44,7 +49,7 @@ export class TodosComponent implements OnInit {
     let innerStrokeColor = '';
 
     let highest = this.getTodoAsPercentage();
-    console.log(highest);
+    // console.log(highest);
 
     if (this.getInProcessTodoAsPercentage() >= highest)
       highest = this.getInProcessTodoAsPercentage();
@@ -112,46 +117,11 @@ export class TodosComponent implements OnInit {
     return percentageVal * this.getTodoCount();
   }
 
-  ngOnInit(): void {
-    this.todoList = [
-      new Todo('Programming Fundamentals'),
-      new Todo('C#'),
-      new Todo('Object-Oriented Programming'),
-      new Todo('SOLID Principles'),
-      new Todo('Understand DRY, KISS, YAGNI'),
-      new Todo('.NET Fundamentals'),
-      new Todo('ASP.NET'),
-      new Todo('MVC'),
-      new Todo('MSSQL'),
-      new Todo('Entity Framework'),
-      new Todo('SignalR'),
-      new Todo('.NET Identity'),
-      new Todo('xUnit'),
-      new Todo('Web API'),
-      new Todo('HTML/CSS'),
-      new Todo('BOOTSTRAP'),
-      new Todo('Tailwind CSS'),
-      new Todo('JavaScript/TypeScript'),
-      new Todo('jQuery'),
-      new Todo('Hate and quit jQuery'),
-      new Todo('Angular'),
-      new Todo('Azure'),
-      new Todo('RabbitMQ'),
-      new Todo('Docker'),
-      new Todo('Kubernetes'),
-      new Todo('Data Structure & Algorithms'),
-      // new Todo('Learn programming'),
-      // new Todo('Learn SOLID principles'),
-      // new Todo('Add new technologies to your toolbox'),
-      // new Todo('Develop projects for the resume'),
-      // new Todo('Update your CV'),
-      // new Todo('Apply for jobs in Europe'),
-      // new Todo('Get no response'),
-      // new Todo('Reach recruiters'),
-      // new Todo(
-      //   'Get rejections like "Thanks for your time. Unfortunately, we are looking for a candidate who currently lives in Europe"'
-      // ),
-      // new Todo('GOTO: 3'),
-    ];
+  loadCareerPathTodos(): void {
+    this.todoList = getCareerPathTodos();
+  }
+
+  loadDotnetFullStackTodos(): void {
+    this.todoList = getDotnetFullStackTodos();
   }
 }
