cf51ea73c4e77916434acd0a1bf64859db5c80dc
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-05-22 16:52:09 +0300

    feat: Procfile and server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + '/dist/todone'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + 'dist/todone', 'index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 5000);
